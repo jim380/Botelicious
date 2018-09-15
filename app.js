8d038e7bb1f065cb01e8a410887501c1fd006231
Jay Jie <jyz1992@hotmail.com>
2018-09-14 22:33:37 -0700

    cleaned up the code. Fixed missing dependencies in package.json

--- app.js
+++ app.js
@@ -1,46 +1,62 @@
-// Load up the discord.js library
+// Load the discord.js library
 const Discord = require("discord.js");
+const got = require('got');
+const cheerio = require('cheerio');
+const { stringify } = require('querystring');
 
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
+//***************************************//
+//                 Client                //
+//***************************************//
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values.
+//***************************************//
+//Load config.json which contains your   //
+//Discord bot token and prefix.          //
+//***************************************//
 const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+
+//***************************************//
+//                 Reply                 //
+//***************************************//
 const responseObject = require("./reply.json");
 
+//***************************************//
+//              Log on message           //
+//***************************************//
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  // Example of changing the bot's playing game to something useful. `client.user` is what the
-  // docs refer to as the "ClientUser".
-  //client.user.setActivity(`Serving ${client.guilds.size} servers`);
     client.user.setActivity(`the game`);
 });
 
+//***************************************//
+//           Bot added to a server       //
+//***************************************//
 client.on("guildCreate", guild => {
-  // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 
 });
 
+//***************************************//
+//         Bot removed from a server     //
+//***************************************//
 client.on("guildDelete", guild => {
-  // this event triggers when the bot is removed from a guild.
   console.log(`Bot has been removed from: ${guild.name} (id: ${guild.id})`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
+
+//***************************************//
+// Logs channel:                         //
+// keeps message  delete history         //
+//***************************************//
 client.on('messageDelete', async (message) => {
   const logs = message.guild.channels.find('name', 'logs');
   if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
     message.guild.createChannel('logs', 'text');
   }
   if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
-    console.log('The logs channel does not exist and tried to create the channel but I am lacking permissions')
+    console.log('Logs channel does not exist! Need permission to create one.')
   }
   const entry = await message.guild.fetchAuditLogs({type: 'MESSAGE_DELETE'}).then(audit => audit.entries.first())
   let user = ""
@@ -61,28 +77,26 @@ client.on("message", (message) => {
   }
 });
 
+//***************************************//
+// Message send:                         //
+// triggers whenever the bot receives    //
+// a message (in channel or DM)          //
+//***************************************//
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
+  // Ignores messages from other bots & itself
   if(message.author.bot) return;
 
-  // Also good practice to ignore any message that does not start with our prefix,
-  // which is set in the configuration file.
+  // Ignores any message that does not start
+  // with the prefix,
   if(message.content.indexOf(config.prefix) !== 0) return;
 
-  // Here we separate our "command" name, and our "arguments" for the command.
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
+  // Separate command and its arguments
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  //*****************************************************************************************//
-  //-----------------------------------------MEX---------------------------------------------//
-  //*****************************************************************************************//
+//-----------------------------------------------------------------------------------------//
+//                                         MEX                                             //
+//-----------------------------------------------------------------------------------------//
     const fetch = require('node-fetch');
     const crypto = require('crypto');
     const qs = require('qs');
@@ -245,9 +259,13 @@ client.on("message", async message => {
           };
       }());
     }
-//*****************************************************************************************//
-//-----------------------------------------END---------------------------------------------//
-//*****************************************************************************************//
+//-----------------------------------------------------------------------------------------//
+//                                         End                                             //
+//-----------------------------------------------------------------------------------------//
+
+//---------------------------------------//
+//            Command examples           //
+//---------------------------------------//
   if(command === "test") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
@@ -347,9 +365,9 @@ client.on("message", async message => {
   }
 
   if (command === "google") {
-          const got = require('got');
-          const cheerio = require('cheerio');
-          const { stringify } = require('querystring');
+          //const got = require('got');
+          //const cheerio = require('cheerio');
+          //const { stringify } = require('querystring');
           if (args.length < 1) message.channel.send('Please enter something for me to search.');
           await message.channel.send('Searching......').then(message => { message.delete(1000) });
           const params = {
